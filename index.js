33407dc834ae39a60167f12343e489e152319b18
André König <andre.koenig@gmail.com>
2014-02-23 19:58:44 +0100

    Initial commit. Created directory structure.

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * bunchitos
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
