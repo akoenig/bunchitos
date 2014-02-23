f079104e755a7998ac044980af7e7dd88fad6938
André König <andre.koenig@gmail.com>
2014-02-23 21:35:52 +0100

    Written unit test.

--- specs/bunchitos.spec.js
+++ specs/bunchitos.spec.js
@@ -13,3 +13,39 @@
 
 'use strict';
 
+var path      = require('path'),
+    bunchitos = require('../');
+
+process.chdir(path.join(__dirname, 'assets'));
+
+describe('The "bunchitos" module loader', function () {
+
+    it('should be able to load modules from "node_modules"', function (done) {
+        var modules = bunchitos(true);
+
+        expect(modules.testModule).toBeDefined();
+        expect(typeof modules.testModule).toBe('function');
+
+        done();
+    });
+
+    it('should be able to load modules from the "package.json"', function (done) {
+        var modules = bunchitos();
+
+        expect(modules.testModule).toBeDefined();
+        expect(typeof modules.testModule).toBe('function');
+
+        done();
+    });
+
+    it('should be able to load only specifiy modules (by prefix)', function (done) {
+        var modules = bunchitos('test-');
+
+        expect(modules.testModule).toBeDefined();
+        expect(typeof modules.testModule).toBe('function');
+
+        expect(modules.express).toBeUndefined();
+
+        done();
+    });
+});
