3a61d2baadfdec8232c1689e75d3df1a7fdafcd6
André König <andre.koenig@gmail.com>
2014-02-23 21:13:05 +0100

    Implemented the core functionality for loading the respective modules.

--- index.js
+++ index.js
@@ -13,3 +13,23 @@
 
 'use strict';
 
+var Bunch = require('./lib').Bunch;
+
+/**
+ * Fetches all modules with the given prefix.
+ *
+ * @param  {string} prefix The module prefix, e.g. gulp-
+ * @param  {boolean} fsOnly Which should be the reference the 'node_modules' or the 'package.json'
+ *
+ * @return {object} An hashmap with the required module.
+ *
+ */
+module.exports = function (prefix, fsOnly) {
+
+    if ('boolean' === typeof prefix) {
+        fsOnly = prefix;
+        prefix = undefined;
+    }
+
+    return Bunch.fetch(prefix, fsOnly);
+};
